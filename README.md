9c2178f02f42b7551b67eb9d991fed679c2b8efa
joemzhao <mengjie.zhao@outlook.com>
2019-02-13 11:55:34 +0100

    better api used

--- README.md
+++ README.md
@@ -1,28 +1,34 @@
 # tweets-retriever
-Retrieve tweets from Twitter API with tweet ID.
+Retrieve tweets from Twitter API via tweet ID.
 
-Retrieving tweet texts for Twitter sentiment classification for 13 European languages. Human annotated sentiment label for each tweet is from [1].
+Retrieving tweet texts for sentiment classification for 15 European languages. Human annotated sentiment label for each tweet is from Mozetic et. al. (2016).
 
-Languages:
- * Albanian
- * Bosnian
- * Bulgarian
- * Croatian
- * English
- * German
- * Hungarian
- * Polish
- * Portuguese
- * Russian
- * Serbian
- * Slovak
- * Slovenian
- * Spanish
- * Swedish
+For each of following languages, around 15k positive/negative Tweets are retrived. No neutral tweets.
 
-The goal is to gradually retrieve 1000 tweets in each of the 13 languages. It consume significant amount of time due to restrictions from the Twitter API rate limit. 
+Languages - ISO 639-3 code:
+ * Albanian - sqi
+ * Bosnian - bos
+ * Bulgarian - bul
+ * Croatian - hrv
+ * English - eng
+ * German - deu
+ * Hungarian - hun
+ * Polish - pol
+ * Portuguese - por
+ * Russian - rus 
+ * Serbian - srp
+ * Slovak - slk
+ * Slovenian -slv
+ * Spanish - spa
+ * Swedish - swe
 
-In total there are 1.6 million tweets. For simplicity all netrual tweets are skipped and output row format is:
+## Portocal
+
+For each language, retrieve annotated tweets from the _same annotator_ for consistency. 
+
+## Format
+
+Retrieved tweets are in follwoing format (per row) :
 
 TweetID \t Polarity \t Tweet-texts
 
@@ -30,9 +36,10 @@ A sample row (Swedish):
 
 508925618311135232	Positive	@BrittaBostrm Är ledig o ensam hemma, såå skönt! 😊☕️☀️💕
 
+
+
 ## References
 ```
-[1]
 @article{DBLP:journals/corr/MozeticGS16,
   author    = {Igor Mozetic and
                Miha Grcar and
--- tweets_retriever.py
+++ tweets_retriever.py
@@ -1,10 +1,9 @@
-from twython import Twython
 from collections import Counter
 
-import tqdm
-import twython
-import time
+import tweepy
+import random
 
+random.seed(3)
 
 CONSUMER_KEY = ""
 CONSUMER_SECRET = ""
@@ -12,22 +11,24 @@ OAUTH_TOKEN = ""
 OAUTH_TOKEN_SECRET = ""
 
 
-class TwitterRetriever(object):
-    def __init__(self):
-        self.twitter = Twython(CONSUMER_KEY, CONSUMER_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
-    
-    def retrive_texts(self, tweet_id):
-        return self.twitter.show_status(id=tweet_id)["text"]
+def batches(it, size):
+	batch = []
+	for item in it:
+		batch.append(item)
+		if len(batch) == size:
+			yield batch
+			batch = []
+	if len(batch) > 0: yield batch 
 
 
 class DatasetLang(object):
     """ For each of the 13 language dataset, use annotated tweets 
     from the most-annotate annotator.
-    Skip all neutral tweets.
+    Skip all neutral tweets and only use 15000 tweets for each language.
     """
     def __init__(self, lang, csvpath):
         self.lang = lang
-        self.id2poar, self.which_annotator, self.max_annotations = self._parse_csv(csvpath)
+        self.id2polar, self.which_annotator, self.max_annotations = self._parse_csv(csvpath)
         print ("[INFO]: ! finish init the dataset object for {} !\n".format(lang))
     
     def _parse_csv(self, csvpath):
@@ -45,44 +46,65 @@ class DatasetLang(object):
             if len(annos) == max_annotations:
                 which_annotator = anno_id
                 break
-        print ("[INFO]: using results from annotator {}, with {} annotations.".format(
+        print ("[INFO]: use results of annotator {}, with {} annotations.".format(
             which_annotator, max_annotations
         ))
         annos = annoor2annoes[which_annotator]
+        random.shuffle(annos)
+        annos = annos[:15000]
         labels = [x[1] for x in annos]
         print ("[INFO]: loading {} valid tweets.".format(len(annos)))
         print ("[INFO]: tweets distribution {}".format(Counter(labels)))
         return dict(annos), which_annotator, max_annotations
 
 
+class TwitterRetriever(object):
+    def __init__(self):
+        auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+        auth.set_access_token(OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+        self.twitter = tweepy.API(
+            auth, 
+            wait_on_rate_limit=True, 
+            wait_on_rate_limit_notify=True
+        )
+    
+    def retrive_texts(self, tweet_ids):
+        tweet_status = self.twitter.statuses_lookup(id_=tweet_ids)
+        return tweet_status
+
+
 class RetrievedData(object):
     OUTPATH = "./retrieved_dataset/"
     def __init__(self, para):
         self.lang, csvpath = para[0], para[1]
+        self.num_written = 0
         self.identifiers = DatasetLang(self.lang, csvpath)
         self.retriever = TwitterRetriever()
         self.retrieve_it()
 
     def retrieve_it(self):
         num_writes, met_id = 0, set()
-        while num_writes < 1000:
-            for myid, mypolar in self.identifiers.id2poar.items():
-                if myid in met_id: continue
-                time.sleep(59)
-                try:
-                    mytext = self.retriever.retrive_texts(myid)
-                except twython.exceptions.TwythonError as e:
-                    print (e)
+        all_ids = self.identifiers.id2polar.keys()
+        for batch_ids in batches(all_ids, 100):
+            mystatus = self.retriever.retrive_texts(batch_ids)
+            batch_id2txt = []
+            for sta in mystatus:
+                batch_id2txt.append((sta.id, sta.text))
+            self.write_batch(batch_id2txt)
+
+    def write_batch(self, batch_id2txt):
+        print (len(batch_id2txt))
+        with open(RetrievedData.OUTPATH + "{}.txt".format(self.lang), "a") as f:
+            for myid, mytext in batch_id2txt:
+                myid = str(myid)
+                if myid not in self.identifiers.id2polar: 
                     continue
-                with open(RetrievedData.OUTPATH+"{}.data".format(self.lang), "a") as f:
-                    f.write("{}\t{}\t{}\n".format(myid, mypolar, mytext.encode("utf-8")))
-                print ("[INFO]: ! writeen following row !")
-                print (self.lang, myid, mypolar, mytext)
-                num_writes += 1
-                met_id.add(myid)
-        print ("[INFO]: finish retrieving tweets for {}".format(self.lang))
+                mypolar = self.identifiers.id2polar[myid]
+                f.write("{}\t{}\t\{}\n".format(myid, mypolar, mytext.encode("utf-8")))
+                self.num_written += 1
+        print ("[INFO]: finish written {} tweets for {}".format(self.num_written, self.lang))
 
 
 if __name__ == "__main__":
     locate = lambda x: "./dataset/{}_Twitter_sentiment.csv".format(x)
-    myretrievers = RetrievedData(("swe", locate("Swedish")))
+    myretrievers = RetrievedData(("eng", locate("English")))
